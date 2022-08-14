4975524990088606dba326d0c33f392e2abb3975
Kunal Singh <singh.kunal1813@gmail.com>
2022-08-14 16:13:15 +0530

    env

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://process.env.SITE_URL/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id, username: profile.provider + profile.id }, function (err, user) {
